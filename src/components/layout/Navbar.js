5b41bb859053f45a2e2a2eee820dffa172c1fb5a
SaulBassoJr <saulbassojr02@gmail.com>
2023-09-28 10:51:16 -0300

    Refactor Navbar into a component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import './main_header.css';
+import './main_action.css';
+
+function Navbar() {
+    return (
+        <header class="main_header">
+            <nav id="menu" class="menu">
+                <a class="main_action" href="index.html"> Home </a>
+                <a class="main_action" href="galeria.html"> Galeria de Criação </a>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
