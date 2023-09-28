20a4aa543d3a7b58390fc0efa97bd362dcde1462
SaulBassoJr <saulbassojr02@gmail.com>
2023-09-28 11:27:53 -0300

    Creates the Footer component

---
+++ src/components/layout/footer/Footer.js
@@ -0,0 +1,11 @@
+import './main_footer.css';
+
+function Footer() {
+    return (
+        <footer class="main_footer">
+            <p>&copy2023 DevSaul DevAlmeida</p>
+        </footer>
+    )
+}
+
+export default Footer
