d84100cd4fe327ad4b8477f13f39ac71f827b066
SaulBassoJr <saulbassojr02@gmail.com>
2023-10-01 10:36:52 -0300

    Create the modalIncer component

---
+++ src/components/layout/modal/modalIncer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './main_modal.css';
+
+const ConfirmationModal = ({ isOpen, closeModal, onConfirm, cardInfo }) => {
+  if (isOpen) {
+    return (
+      <div className="main_modal" >
+        <div className="wrapper_modal">
+          <div>
+            <button className="closeButton" onClick={closeModal}>&times;</button>
+          </div>
+          <div className='titleModal'>
+           <h1 >Deseja mover o cartão para a Prancha?</h1>
+          </div>
+          <div className="wrapper_insputs -mprancha">
+            <div className='styleCard' style={{ backgroundColor: cardInfo.backgroundColor }} >
+              <img src={cardInfo.imgSrc} alt="Pré-visualização" />
+              <h2>{cardInfo.title}</h2>
+            </div>
+            {cardInfo.audioSrc ? (
+              <div>
+                <audio className='inputs' controls>
+                  <source src={cardInfo.audioSrc} type="audio/mp3" />
+                  Seu navegador não suporta o elemento de áudio.
+                </audio>
+              </div>
+            ) : (
+              <p>Áudio não disponível</p>
+            )}
+            <button className="styleButton" onClick={() => { onConfirm(); closeModal(); }}>Confirmar</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return null;
+};
+
+export default ConfirmationModal;
